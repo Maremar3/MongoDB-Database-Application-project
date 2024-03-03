6aa58c78aa4467a3010d0cead231485c0e3029ea
Maryana Ghabyous <maremar3@hotmail.com>
2024-03-03 00:49:30 -0700

    create the Json file has documents

--- routes/emp.mjs
+++ routes/emp.mjs
@@ -4,14 +4,4 @@ import db from '../db/conn.mjs';
 const router = express.Router();
 let collection = await db.collection("emp");
 
-  let result = await collection.aggregate([
-    {
-        $set: {"id": 20}
-                }
-            ])
-
-// db.emp.aggregate( [
-//     { $set: { "id": 20 } }
-//   ] )
-
 export default router;
